6a1ceb8237453edb116443b36e1870b532f1c4f1
YOUR NAME <EMAIL ADDRESS>
2026-04-23 14:24:56 +0200

    Enhanced Shell apps building, all system calls and system-related features migrated into a clean standart lib

--- shell/hello.cpp
+++ shell/hello.cpp
@@ -1,24 +1,5 @@
-// Programme test — syscalls DOS64
-#define SYS_PRINTLN 1
-#define SYS_EXIT    4
 
-static void sys_println(const char* s) {
-    asm volatile(
-        "mov $1, %%rax\n"    // SYS_PRINTLN
-        "mov %0, %%rdi\n"    // argument = string
-        "int $0x80\n"
-        :: "r"(s) : "rax", "rdi"
-    );
-}
-
-static void sys_exit(int code) {
-    asm volatile(
-        "mov $4, %%rax\n"
-        "mov %0, %%rdi\n"
-        "int $0x80\n"
-        :: "r"((long long)code) : "rax", "rdi"
-    );
-}
+#include "std.h"
 
 extern "C" int main() {
     sys_println("Hello from ELF64!");
